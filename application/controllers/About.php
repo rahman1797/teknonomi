f6dbbde948b9a935afbfb90426a7ab650da0fbae
sauliakarina <sauliakarina@gmail.com>
2019-08-24 09:00:55 +0700

    update about

--- application/controllers/About.php
+++ application/controllers/About.php
@@ -3,12 +3,14 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class About extends CI_Controller {
 	 function __construct(){
-	    parent::__construct();    
+	    parent::__construct();
+	    $this->load->model('m_user');    
   }
 
 	public function index()
 	{
 		$data = array(
+			'user' => $this->m_user->getUser()
 		);
 		$this->load->view('element/head');
 		$this->load->view('element/header');
